0d172c9f8a73f8b0f517d6c9e94838af33e69865
i.rusanov <rusanov.qi@gmail.com>
2026-05-10 16:35:16 +0700

    feat(ai-agent): add service module

--- ai-agent/src/test/java/backend/academy/linktracker/ai/TestAiAgentApplication.java
+++ ai-agent/src/test/java/backend/academy/linktracker/ai/TestAiAgentApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 
 public class TestAiAgentApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.from(AiAgentApplication::main)
                 .with(TestcontainersConfiguration.class)
                 .run(args);
